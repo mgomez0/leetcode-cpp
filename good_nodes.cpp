df8e67b5a34bb94a638dddcbad9e5b29dfa45759
Michael Gomez <michaelgomez@csus.edu>
2020-08-26 19:43:35 -0700

    add new path_sum

--- good_nodes.cpp
+++ good_nodes.cpp
@@ -4,10 +4,25 @@
 // Return the number of good nodes in the binary tree.
 
 #include "TreeNode.h"
-
+#include <vector>
+#include <algorithm>
 class Solution {
-    public:
-        int goodNodes(TreeNode* root){
-            
+public:
+    int num_good;
+    int goodNodes(TreeNode* root) {
+        std::vector<int> path_vector;
+        dfs(root, path_vector);
+    }
+
+    void dfs(TreeNode* root, std::vector<int> path_vector){
+        if(!root){return;}
+        path_vector.push_back(root->val);
+        for(int i : path_vector){
+            if(i > *std::max_element(path_vector.begin(), path_vector.end())){
+                
+            }
         }
-}
+
+
+    }
+};
